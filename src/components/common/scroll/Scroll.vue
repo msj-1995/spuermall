c63c695d7d34ecd0d66d76da79802d24bde6e17c
msj <2669159659@qq.com>
2021-07-28 21:11:39 +0800

    better-scroll应用到首页

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -1,10 +1,28 @@
 <template>
-
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
+import BScroll from "better-scroll";
+
 export default {
-  name: "Scroll"
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    // 使用querySelector获取标签不太合适，如果项目中有多个相同的class名，则通过querySelector拿到的是第一个标签，有时候我们不知道哪一个是第一个，就会拿错
+    // vue中可以通过ref明确的拿到某一个标签
+    this.scroll = new BScroll(this.$refs.wrapper, {
+
+    })
+  }
 }
 </script>
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,7 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <scroll>
+    <scroll class="content">
       <home-swiper :banners="banners"/>
       <recommend-view :recommends="recommends"/>
       <feature-view/>
@@ -12,139 +12,138 @@
                    @tabClick="tabClick"/>
       <goods-list :goods="showGoods"/>
     </scroll>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-    </ul>
   </div>
 </template>
 
 <script>
-  // 导入NavBar组件:分类导入，并且在注册组件时，保持和导入的顺序一致，方便以后管理。
-  import HomeSwiper from "./childComps/HomeSwiper";
-  import RecommendView from "./childComps/RecommendView";
-  import FeatureView from "./childComps/FeatureView";
+// 导入NavBar组件:分类导入，并且在注册组件时，保持和导入的顺序一致，方便以后管理。
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
-  import NavBar from "components/common/navbar/NavBar";
-  import TabControl from "components/content/tabControl/TabControl";
-  import GoodsList from "components/content/goods/GoodsList";
-  import Scroll from "components/common/scroll/Scroll";
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
 
-  import {getHomeMultiData, getHomeGoods} from "network/home";
+import {getHomeMultiData, getHomeGoods} from "network/home";
 
-  export default {
-    name: "Home",
-    components: {
-      HomeSwiper,
-      RecommendView,
-      FeatureView,
-      NavBar,
-      TabControl,
-      GoodsList,
-      Scroll
-    },
-    data() {
-      return {
-        // result: null
-        banners: [],
-        recommends: [],
-        goods: {
-          'pop': {page: 0, list: []},
-          'new': {page: 0, list: []},
-          'sell': {page: 0, list: []}
-        },
-        currentType: 'pop'
-      }
-    },
-    created() {
-      // 1、请求多个数据
-      this.getHomeMultiData()
-
-      // 2.商品数据请求:this.函数名,调用的是vue methods中的函数，不使用this时表示的是使用import导入的函数
-      this.getHomeGoods('pop')
-
-      this.getHomeGoods('new')
-
-      this.getHomeGoods('sell')
-    },
-    computed: {
-      showGoods() {
-        return this.goods[this.currentType].list
-      }
-    },
-    methods: {
-      /**
-       * 事件监听相关方法
-       */
-      tabClick(index) {
-        switch(index) {
-          case 0:
-            this.currentType = 'pop'
-            break
-          case 1:
-            this.currentType = 'new'
-            break
-          case 2:
-            this.currentType = 'sell'
-            break
-        }
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodsList,
+    Scroll
+  },
+  data() {
+    return {
+      // result: null
+      banners: [],
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
       },
+      currentType: 'pop'
+    }
+  },
+  created() {
+    // 1、请求多个数据
+    this.getHomeMultiData()
 
-      /**
-       * 网络请求相关方法
-       */
-      getHomeMultiData() {
-        getHomeMultiData().then(res => {
-          this.banners = res.data.banner.list;
-          this.recommends = res.data.recommend.list;
-        })
-      },
-      getHomeGoods(type) {
-        const page = this.goods[type].page + 1
-        getHomeGoods(type, page).then(res => {
-          // 数据的解构：它会把我们从服务器取到的那一页的数组列表一个一个解构出来然后在放到goods中的list中
-          this.goods[type].list.push(...res.data.list)
-          this.goods[type].page += 1
-        })
+    // 2.商品数据请求:this.函数名,调用的是vue methods中的函数，不使用this时表示的是使用import导入的函数
+    this.getHomeGoods('pop')
+
+    this.getHomeGoods('new')
+
+    this.getHomeGoods('sell')
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
+    }
+  },
+  methods: {
+    /**
+     * 事件监听相关方法
+     */
+    tabClick(index) {
+      switch(index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
       }
+    },
+
+    /**
+     * 网络请求相关方法
+     */
+    getHomeMultiData() {
+      getHomeMultiData().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        // 数据的解构：它会把我们从服务器取到的那一页的数组列表一个一个解构出来然后在放到goods中的list中
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
     }
   }
+}
 </script>
 
+<!--
+  style中设置scoped的好处，该组件中的样式只对本组件中的html样式标签起作用，如果没有scoped可能有影响其他组件同名class下的样式
+-->
 <style scoped>
-  /*如果使用导航栏固定位置，则轮播图会与顶部对齐，从而轮播图会被导航栏遮挡掉一部分，因此把home整体下拉一点*/
-  #home {
-    padding-top: 44px;
-  }
-  .home-nav {
-    background-color: var(--color-tint);
-    color: #e9e9e9;
+/*如果使用导航栏固定位置，则轮播图会与顶部对齐，从而轮播图会被导航栏遮挡掉一部分，因此把home整体下拉一点*/
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #e9e9e9;
 
-    /*让导航栏不滚动*/
-    position: fixed !important;
-    left: 0;
-    right: 0;
-    top: 0;
-    z-index: 99;
-  }
-  /*简单实现tabControl（流行，新款，精选）的吸附功能
-  这里使用css实现：使用position中的sticky属性
-  */
-  .tab-control {
-    position: sticky;
-    top: 44px;
-  }
+  /*让导航栏不滚动*/
+  position: fixed !important;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 99;
+}
+/*简单实现tabControl（流行，新款，精选）的吸附功能
+这里使用css实现：使用position中的sticky属性
+*/
+.tab-control {
+  position: sticky;
+  top: 44px;
+}
+
+.content {
+  /*height: 300px;*/
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+
 </style>
