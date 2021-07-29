fb764a36810a1b9ba46819dc91cf75663270b0e1
msj <2669159659@qq.com>
2021-07-29 23:26:26 +0800

    详情也数据请求与轮播图展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,25 +2,38 @@
   <div id="detail">
     <!--导航-->
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+
+import {getDetail} from "@/network/detail";
 
 export default {
   name: "Detail",
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   },
   // 组件创建后获取并保存iid
   created() {
+    // 1.保存传入的iid
     this.iid = this.$route.params.iid
+
+    // 2.根据iid请求详情数据
+    getDetail(this.iid).then(res => {
+      // 1. 获取顶部图片的轮播数据
+      this.topImages = res.result.itemInfo.topImages
+    })
   }
 }
 </script>
