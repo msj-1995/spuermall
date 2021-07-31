5b3a7ecec33b357ba0ad3372ae23e3c3af5b2c17
msj <2669159659@qq.com>
2021-07-31 23:24:53 +0800

    购物车商品列表展示雏形

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,20 +1,28 @@
 <template>
  <div class="cart">
-   <nav-bar>
+   <!--导航-->
+   <nav-bar class="nav-bar">
      <div slot="center">购物车({{length}})</div>
    </nav-bar>
+
+   <!--商品列表-->
+   <cart-list></cart-list>
+
+   <!--底部汇总-->
  </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import CartList from "./childComps/CartList";
 
 import {mapGetters} from 'vuex'
 
 export default {
   name: "Cart",
   components: {
-    NavBar
+    NavBar,
+    CartList
   },
   computed: {
     ...mapGetters({
@@ -25,6 +33,9 @@ export default {
 </script>
 
 <style scoped>
+.cart {
+  height: 100vh;
+}
 .nav-bar {
   background-color: var(--color-tint);
   /*背景色:字体颜色*/
---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cart-list">
+    <scroll class="content" ref="scroll">
+      <cart-list-item v-for="(item, index) in cartList"
+                      :key="index"
+                      :product="item">
+
+      </cart-list-item>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from "components/common/scroll/Scroll";
+import CartListItem from './CartListItem'
+
+import {mapGetters} from 'vuex'
+
+export default {
+  name: "CartList",
+  components: {
+    Scroll,
+    CartListItem
+  },
+  computed: {
+    ...mapGetters(['cartList'])
+  },
+  activated() {
+    // 进入组件时刷新scroll的可滚动高度，以保证在添加数据后可滚动的高度时正确的
+    this.$refs.scroll.refresh()
+  }
+}
+</script>
+
+<style scoped>
+.cart-list {
+  height: calc(100% - 44px - 49px);
+}
+
+.content {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
