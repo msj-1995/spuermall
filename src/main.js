85d2db923e0fd4b84beb12101ffa43a39cd86860
msj <2669159659@qq.com>
2021-08-04 10:51:05 +0800

    解决移动端300毫秒延迟问题

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import store from "./store/index";
 import router from "@/router";
 
 import toast from 'components/common/toast'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
 // 添加事件总线,并且把该事件总线设置为Vue实例，应为vue实例可以发射事件并且可以作为事件总线
 Vue.prototype.$bus = new Vue
+
+// 解决移动端的300ms延迟问题
+fastclick.attach(document.body)
 
 // 安装toast插件:本质是执行toast中对象.install函数
 // 安装插件：执行了Vue.use()后，该插件就准备好了，之后可以随时使用
